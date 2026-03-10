4235ee766ec8e0089b548719ee597735dadd8cb4
Volodymyr Kuzmych <44097750+vvkuzmych@users.noreply.github.com>
2026-03-10 11:34:16 +0200

    refactoring serializers

--- spec/serializers/category_serializer_spec.rb
+++ spec/serializers/category_serializer_spec.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  name        :string
+#  position    :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  parent_id   :integer
+#
+# Indexes
+#
+#  index_categories_on_name  (name) UNIQUE
+#
 require "rails_helper"
 
 RSpec.describe CategorySerializer do
